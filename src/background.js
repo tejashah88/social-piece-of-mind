beb54074ffe6cd448a6060aed03944620ce51c78
Tejas Shah <tejashah88@gmail.com>
2023-03-04 10:54:46 -0800

    added website check for facebook.com/messages

--- src/background.js
+++ src/background.js
@@ -1,7 +1,12 @@
 chrome.tabs.onUpdated.addListener(
     function (tabId, changeInfo, tab) {
         // Only look for tabs with messenger.com and are fully loaded
-        if (tab.url.startsWith('https://www.messenger.com') && changeInfo.status && changeInfo.status == 'complete') {
+        if (
+            tab.url.startsWith('https://www.messenger.com') &&
+            tab.url.startsWith('https://www.facebook.com/messages') &&
+            changeInfo.status &&
+            changeInfo.status == 'complete'
+        ) {
             chrome.tabs.sendMessage(tabId, {
                 message: 'activate-piece-of-mind',
             });
